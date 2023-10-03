2810f733bfe59f29630d95ea664adf6ef92f5ad2
diegorod <diegorod@mit.edu>
2023-10-03 16:16:24 -0400

    added movement buttons

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     public KeyCode upKey, downKey, leftKey, rightKey;
+    
     void Start()
     {
         
@@ -16,9 +17,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(upKey)){
-            
+        if (Input.GetKey(upKey)){
+            transform.position+= Vector3.up*0.005f;
+        }
+        if (Input.GetKey(downKey)){
+            transform.position+= Vector3.down*0.005f;
+        }
+        if (Input.GetKey(leftKey)){
+            transform.position+= Vector3.left*0.005f;
+        }
+        if (Input.GetKey(rightKey)){
+            transform.position+= Vector3.right*0.005f;
         }
     }
 }
