bf37439746c5651fc5b480a55e74a556429d717a
diegorod <diegorod@mit.edu>
2023-10-05 14:33:37 -0400

    changed scale

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,16 +18,16 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(upKey)){
-            transform.position+= Vector3.up*0.005f;
+            transform.position+= Vector3.up*0.01f;
         }
         if (Input.GetKey(downKey)){
-            transform.position+= Vector3.down*0.005f;
+            transform.position+= Vector3.down*0.01f;
         }
         if (Input.GetKey(leftKey)){
-            transform.position+= Vector3.left*0.005f;
+            transform.position+= Vector3.left*0.01f;
         }
         if (Input.GetKey(rightKey)){
-            transform.position+= Vector3.right*0.005f;
+            transform.position+= Vector3.right*0.01f;
         }
     }
 }
