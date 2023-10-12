c932cdd2ea0d5d1cccaa8b96e466f6fc11e3a93f
Sam Bruce <sgbruce@mit.edu>
2023-10-12 13:54:14 -0400

    map updates

--- Assets/Scripts/MapObjects.cs
+++ Assets/Scripts/MapObjects.cs
@@ -5,30 +5,54 @@ using UnityEngine;
 public class MapObjects : MonoBehaviour
 {
     public GameObject log; 
-    float framePos = 0.1f;
-    const int numBorders = 10;
+    int framePos = 0;
+    const int numBorders = 30;
+    const int numObstacles = 50;
+    int currentObstacle = 0;
     GameObject[] borders = new GameObject[numBorders];
+    GameObject[] obstacles = new GameObject[numObstacles];
     // Start is called before the first frame update
     void Start()
     {
         //Instantiate(log, new Vector3(2.0f, 0, 0), Quaternion.identity);
-        borders[numBorders - 1] = Instantiate(log, new Vector3(0, 23.0f, 0), new Quaternion(180,0,0,0));
-        borders[numBorders - 2] = Instantiate(log, new Vector3(0, -23.0f, 0), Quaternion.identity);
+        for(int i = 0; i < numBorders; i += 2){
+            float x_pos = -40.0f + (90.0f / ((float)numBorders)) * i;
+            borders[i] = Instantiate(log, new Vector3(x_pos, 23.0f, 0), new Quaternion(180,0,0,0));
+            borders[i + 1] = Instantiate(log, new Vector3(x_pos, -23.0f, 0), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(framePos % 6 < 0.01){
+        if(framePos == 300){
+            framePos = 0;
             Destroy(borders[0]);
             Destroy(borders[1]);
             for(int i = 0; i < numBorders - 2; i+=2){
                 borders[i] = borders[i + 2];
                 borders[i + 1] = borders[i + 3];
             }
-            borders[numBorders - 1] = Instantiate(log, new Vector3(0, 23.0f, 0), new Quaternion(180,0,0,0));
-            borders[numBorders - 2] = Instantiate(log, new Vector3(0, -23.0f, 0), Quaternion.identity);
+            borders[numBorders - 1] = Instantiate(log, new Vector3(44.0f, 23.0f, 0), new Quaternion(180,0,0,0));
+            borders[numBorders - 2] = Instantiate(log, new Vector3(44.0f, -23.0f, 0), Quaternion.identity);
         }
-        framePos += 0.02f;
+        int numDeleted = 0;
+        for(int i = 0; i < numObstacles; i++){
+            if(obstacles[i] && obstacles[i].transform.position.x < -50.0f){
+                Destroy(obstacles[i]);
+                numDeleted++;
+            }
+        }
+        if(numDeleted > 0){
+            currentObstacle -= numDeleted;
+            for(int i = 0; i < numObstacles - numDeleted; i++){
+                obstacles[i] = obstacles[i + numDeleted];
+            }
+        }
+        if(framePos % 100 == 0  && Random.Range(0.0f, 1.0f) < 0.5f && currentObstacle != numObstacles){
+            obstacles[currentObstacle] = Instantiate(log, new Vector3(50.0f, Random.Range(-19.0f, 19.0f), 0), new Quaternion(Random.Range(0,180),Random.Range(0,180),0,0));
+            currentObstacle++;
+        }
+        framePos ++;
     }
 }
