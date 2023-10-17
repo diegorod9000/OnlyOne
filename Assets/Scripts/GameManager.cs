e21ff9c70aaa1f36950023463f6d19627a29b39f
diegorod <diegorod@mit.edu>
2023-10-17 15:52:19 -0400

    added player 2 back sprite, adjusted collision boxes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    static bool frozen;
+    public static bool frozen;
     // Start is called before the first frame update
     void Start()
     {
